22f8d4a53e18222ab2985efc74325faaea0da0d6
Julia Herron <julia.herron@gmail.com>
2016-05-18 10:47:51 -0700

    exercises 1 thru 9

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print "Hello World!"
+print "Yay! Printing."
+
+my_name = "Julia"
+print "My name is %s" % my_name
+
+x = 10.2
+print "This is r : %r" % x
+print "This is d : %d" % x
+print "This is s : %s" % x
